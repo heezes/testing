619ba8570bb846f2b7886300c7ad89d1557a0f61
Altamash <warlock.unleash@gmail.com>
2021-02-12 00:26:32 +0530

    Minor Change: Corrected the repsonse array system in parser utility

--- parser_util.py
+++ parser_util.py
@@ -20,9 +20,9 @@ OTA_SUCCES_RESPONSE         =   3
 
 class response_parser():
     def __init__(self):
-        self.responseArray = {}
+        self.event_obj = []
         for i in range(TOTAL_RESPONSES):
-            self.responseArray[i].event_obj = threading.Event()
+            self.event_obj.append(threading.Event())
 
     def log_and_parse(self, queue):
         if(queue.empty() == False):
@@ -31,29 +31,29 @@ class response_parser():
             # resp = response_string+"  %d"%response_string.find(UNLOCK_OPCODE)
             # print(resp)
             if(response_string.find(UNLOCK_OPCODE) > -1):
-                self.responseArray[UNLOCK_RESPONSE].event_obj.set()
+                self.event_obj[UNLOCK_RESPONSE].set()
             elif (response_string.find(LOCK_OPCODE) > -1):
-                self.responseArray[LOCK_RESPONSE].event_obj.set()
+                self.event_obj[LOCK_RESPONSE].set()
             elif (response_string.find(SYNC_TRIGGERED) > -1):
-                self.responseArray[SYNC_TRIGGERED_RESPONSE].event_obj.set()
+                self.event_obj[SYNC_TRIGGERED_RESPONSE].set()
             elif (response_string.find(SYNC_ENDED) > -1):
-                self.responseArray[SYNC_ENDED_RESPONSE].event_obj.set()
+                self.event_obj[SYNC_ENDED_RESPONSE].set()
             elif (response_string.find(OTA_SUCCES) > -1):
-                self.responseArray[OTA_SUCCES_RESPONSE].event_obj.set()
+                self.event_obj[OTA_SUCCES_RESPONSE].set()
 
 
     def getUnlockResult(self, wait_time):
-        if(self.responseArray[UNLOCK_RESPONSE].event_obj.wait_time(wait_time) == True):
+        if(self.event_obj[UNLOCK_RESPONSE].wait_time(wait_time) == True):
             return True
         return False
 
     def getLockResult(self, wait_time):
-        if(self.responseArray[LOCK_RESPONSE].event_obj.wait_time(wait_time) == True):
+        if(self.event_obj[LOCK_RESPONSE].wait_time(wait_time) == True):
             return True
         return False
 
     def getSyncResult(self, wait_time):
-        if(self.responseArray[SYNC_TRIGGERED_RESPONSE].event_obj.wait_time(wait_time) == True):
-            if(self.responseArray[SYNC_ENDED_RESPONSE].event_obj.wait_time(wait_time) == True):
+        if(self.event_obj[SYNC_TRIGGERED_RESPONSE].wait_time(wait_time) == True):
+            if(self.event_obj[SYNC_ENDED_RESPONSE].wait_time(wait_time) == True):
                 return True
         return False
