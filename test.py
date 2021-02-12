8003e042a40376e75bd8d047e424f092bc16f4eb
heezes <warlock.unleash@gmail.com>
2021-02-13 03:12:57 +0530

    af

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import pygit
+import os
 
+os.chdir('/home/pi/Desktop/testing')
+pygit.update()
 pygit.repos()
 r = pygit.load('testing')
-r.message = "Hello from testing/main.py"
+#r.message = "Hello from testing/main.py"
 file = open("testFile.txt", 'w')
 file.write("Whatsup?")
 file.close()
