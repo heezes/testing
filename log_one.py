2cf9a7a34600bd86767b1b20c121190f15186ae0
heezes <warlock.unleash@gmail.com>
2021-02-17 22:37:33 +0530

    Log file Syncing(Push made from testing/main.py)

--- log_one.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-class my_log_one():
-    def __init__(self):
-        self.log = logging.getLogger('test_one_logger')
-        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
-    def print_info(self):
-        self.log.debug("Yo Yo Bitches I'm here!")
--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #sudo python3 main.py --device_addr "2818117-893931543"
-sudo openocd -f ./flasher/debug.cfg & sudo python3 main.py --device_addr "2818117-893931543" && fg
+sudo openocd -f /home/pi/Desktop/flasher/debug.cfg & sudo python3 main.py --device_addr "2818117-893931543" && fg
