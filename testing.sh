12c3b719798faf84fa9dc801612d610e07affad5
heezes <warlock.unleash@gmail.com>
2021-02-18 12:06:55 +0530

    Log file Syncing(Push made from testing/main.py)

--- testing.sh
+++ testing.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #sudo python3 main.py --device_addr "2818117-893931543"
-sudo openocd -f /home/pi/Desktop/flasher/debug.cfg & sudo python3 main.py --device_addr "2818117-893931543" && fg
+#sudo openocd -f /home/pi/Desktop/flasher/debug.cfg & sudo python3 main.py --device_addr "2621513-893931545" && fg
+sudo python3 main.py --device_addr "2621513-893931545"
