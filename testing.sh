a2f73a91031d8f6f4af2a1eedbad8c7f992361e1
Altamash Abdul Rahim <warlock.unleash@gmail.com>
2021-06-28 15:26:35 +0530

    Major Changes
    [#]Now trigger debug.sh from testing.sh

--- testing.sh
+++ testing.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 
 #sudo python3 main.py --device_addr "2818117-893931543"
-sudo openocd -f /home/pi/Desktop/flasher/src/debug.cfg & sudo python3 main.py --device_addr "2621513-893931545"  --ble_interface True && fg
+source /home/pi/Desktop/flasher/src/debug.sh & sudo python3 main.py --device_addr "2621513-893931545"  --ble_interface True && fg
 #sudo python3 main.py --device_addr "2621513-893931545" --ble_interface False &
