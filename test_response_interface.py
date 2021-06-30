2195eb1dc82bc2c40f5055db1df40f2a56cb169f
Altamash Abdul Rahim <warlock.unleash@gmail.com>
2021-06-30 11:00:12 +0530

    Minor Changes
    [#]Added print logs for rtt data

--- test_response_interface.py
+++ test_response_interface.py
@@ -152,7 +152,7 @@ class interface():
                 try:
                     self.data_queue[1].put((data,))
                     self.logger.debug(data)
-                    # print(data)
+                    print(data)
                 except Exception as e:
                     print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
 
